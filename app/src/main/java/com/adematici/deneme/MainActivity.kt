ed325a9f627d79ded78ae351ef02d580afb66f07
Adem <adematicis41@gmail.com>
2022-04-01 15:45:08 +0300

    Initial commit

---
+++ app/src/main/java/com/adematici/deneme/MainActivity.kt
@@ -0,0 +1,19 @@
+package com.adematici.deneme
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.adematici.deneme.databinding.ActivityMainBinding
+
+class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        binding.textView.text = "asdasd"
+    }
+
+}
