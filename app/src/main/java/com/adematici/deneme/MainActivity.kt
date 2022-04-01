256c5ecdba3f83c2dd95116dcc2c77232057c811
Adem <adematicis41@gmail.com>
2022-04-01 16:24:25 +0300

    Use Member adapter in Main Activity

--- app/src/main/java/com/adematici/deneme/MainActivity.kt
+++ app/src/main/java/com/adematici/deneme/MainActivity.kt
@@ -7,13 +7,20 @@ import com.adematici.deneme.databinding.ActivityMainBinding
 class MainActivity : AppCompatActivity() {
 
     private lateinit var binding: ActivityMainBinding
+    private lateinit var adapter: MemberAdapter
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
-        binding.textView.text = "asdasd"
+        adapter = MemberAdapter()
+        updateMemberList(memberListGeneral)
+    }
+
+    private fun updateMemberList(memberList: ArrayList<Member>) {
+        adapter.memberList = memberList
+        binding.recyclerView.adapter = adapter
     }
 
 }
