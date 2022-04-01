729c9ddb3bd91959ab4b09fd8dbd58573f913350
Adem <adematicis41@gmail.com>
2022-04-01 16:22:07 +0300

    Create Member model class

---
+++ app/src/main/java/com/adematici/deneme/Member.kt
@@ -0,0 +1,23 @@
+package com.adematici.deneme
+
+data class Member(
+    var name: String
+)
+
+val memberListGeneral: ArrayList<Member> = arrayListOf(
+    Member("Adem"),
+    Member("Bulut"),
+    Member("Kutay"),
+    Member("Adem"),
+    Member("Bulut"),
+    Member("Kutay"),
+    Member("Adem"),
+    Member("Bulut"),
+    Member("Kutay"),
+    Member("Adem"),
+    Member("Bulut"),
+    Member("Kutay"),
+    Member("Adem"),
+    Member("Bulut"),
+    Member("Kutay"),
+)
